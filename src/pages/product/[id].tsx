87e27ab0c7bcc205f71b65889fea75c65f9f9a0d
Rodrigo Mota Mendes <rdrmendes@hotmail.com>
2023-06-08 16:36:42 -0300

    Estrutura da página de produto

--- src/pages/product/[id].tsx
+++ src/pages/product/[id].tsx
@@ -1,7 +1,30 @@
 import { useRouter } from 'next/router';
+import {
+  ImageContainer,
+  ProductContainer,
+  ProductDetails,
+} from '@/src/styles/pages/product';
 
 export default function Product() {
   const { query } = useRouter();
 
-  return <h1>Product {JSON.stringify(query)}</h1>;
+  return (
+    <ProductContainer>
+      <ImageContainer></ImageContainer>
+
+      <ProductDetails>
+        <h1>Camiserta X</h1>
+        <span>R$ 79,90</span>
+
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusantium
+          nemo facere porro unde cupiditate tenetur sunt, id, sed aliquid
+          temporibus, quasi aut quod debitis labore accusamus pariatur! Debitis,
+          quo libero.
+        </p>
+
+        <button>Comprar agora</button>
+      </ProductDetails>
+    </ProductContainer>
+  );
 }
